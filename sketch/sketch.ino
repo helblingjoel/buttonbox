832379067512bb5c3adf822ea8be8519fed4b885
joel <joel.helbling1@nhs.net>
2021-12-19 17:55:14 +0000

    Added basic action to button presses

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,11 +1,32 @@
 
 #include <Keyboard.h>
 int buttonValue;
+int buttonPressCoolDownms = 500;
 void setup() {
   Keyboard.begin();
 }
 
 void loop() {
-  buttonValue = 0;
   buttonValue = analogRead(A0);
+
+  if (buttonValue >= 200 && buttonValue <= 220) {
+    buttonPress(0, analogRead(A0));
+  }
+  else if (buttonValue >= 240 && buttonValue <= 280) {
+    buttonPress(1, analogRead(A0));
+  }
+  else if (buttonValue >= 300 && buttonValue <= 400) {
+    buttonPress(2, analogRead(A0));
+  }
+  else if (buttonValue >= 490 && buttonValue <= 550) {
+    buttonPress(3, analogRead(A0));
+  }
+  else if (buttonValue >= 1000) {
+    buttonPress(4, analogRead(A0));
+  }
+}
+
+void buttonPress(int buttonId, int entryReading) {
+  Serial.println(buttonId);
+  delay(buttonPressCoolDownms);
 }
